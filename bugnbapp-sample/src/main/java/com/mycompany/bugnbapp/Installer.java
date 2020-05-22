4a581d804f84e328d7ada9af2c200edd8e2d9aa3
Emilian Bold <emi@apache.org>
2020-05-22 13:11:50 +0300

    Installer wizzard output

---
+++ bugnbapp-sample/src/main/java/com/mycompany/bugnbapp/Installer.java
@@ -0,0 +1,12 @@
+package com.mycompany.bugnbapp;
+
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        // TODO
+    }
+
+}
