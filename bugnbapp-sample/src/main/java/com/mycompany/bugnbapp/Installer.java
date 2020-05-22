c956deedf63c71b0d9c4fdf0fa0959d494d83bcf
Emilian Bold <emi@apache.org>
2020-05-22 13:22:03 +0300

    Implementation dependency to org-netbeans-core-network (which does not work...)

--- bugnbapp-sample/src/main/java/com/mycompany/bugnbapp/Installer.java
+++ bugnbapp-sample/src/main/java/com/mycompany/bugnbapp/Installer.java
@@ -1,12 +1,20 @@
 package com.mycompany.bugnbapp;
 
+import org.netbeans.core.network.utils.HostnameUtils;
+import org.netbeans.core.network.utils.NativeException;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
 
 public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
-        // TODO
+        try {
+            String host = HostnameUtils.getNetworkHostname();
+            System.out.println("Hostname is " + host);
+        } catch (NativeException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 
 }
